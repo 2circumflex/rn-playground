b91b919824a3b00be276b5a2dbbb34535292dabb
Roy <2circumflex@gmail.com>
2025-01-16 02:51:38 +0900

    expo-image-manipulator 추가 및 flip 이미지

--- create-video-test/app/detail.tsx
+++ create-video-test/app/detail.tsx
@@ -1,6 +1,13 @@
 import { Button, View, StyleSheet } from "react-native";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import { Image } from "expo-image";
+import {
+  FlipType,
+  SaveFormat,
+  ImageManipulator,
+  useImageManipulator,
+} from "expo-image-manipulator";
+import { useState } from "react";
 
 type DetailParams = {
   photoUri: string;
@@ -9,11 +16,27 @@ type DetailParams = {
 export default function Detail() {
   const router = useRouter();
   const params = useLocalSearchParams<DetailParams>();
+  const imageManipulator = useImageManipulator(params.photoUri);
+
+  const [flippedImage, setFlippedImage] = useState<string | null>(null);
+
+  const _rotate90andFlip = async () => {
+    const context = imageManipulator.flip(FlipType.Horizontal);
+    const image = await context.renderAsync();
+    const result = await image.saveAsync({ format: SaveFormat.JPEG });
+
+    console.log("Flipped image:", result);
+    setFlippedImage(result.uri);
+  };
 
   return (
     <View style={styles.container}>
       <Image source={{ uri: params.photoUri }} style={styles.image} />
+      {flippedImage && (
+        <Image source={{ uri: flippedImage }} style={styles.image} />
+      )}
       <Button onPress={() => router.back()} title="뒤로가기" />
+      <Button onPress={_rotate90andFlip} title="90도 회전 후 뒤집기" />
     </View>
   );
 }
